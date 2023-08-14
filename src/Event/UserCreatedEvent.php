ddfbdc740345158435e63e816c2f64c8c6fcd1ad
Jaime <jaime@osmos.mx>
2023-08-14 09:23:53 -0600

    Added CLI stage

---
+++ src/Event/UserCreatedEvent.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
+ * @version 1
+ * @since 1
+ * Created at 13/08/23 10:19
+ */
+
+namespace App\Event;
+
+class UserCreatedEvent
+{
+
+}
